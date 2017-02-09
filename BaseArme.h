eaa8e6c2e45906767f8dccb2d415acad2f89815b
Zilmerx <hex404@gmail.com>
2017-02-08 23:58:32 -0500

    ad

---
+++ BaseArme.h
@@ -0,0 +1,46 @@
+#pragma once
+
+
+
+class Arme
+{
+	virtual int damage() = 0;
+};
+
+class LongSword : public Arme
+{
+	virtual int damage() override
+	{
+		return 10;
+	}
+
+};
+
+class LongSword : public Arme
+{
+	virtual int damage() override
+	{
+		return 10;
+	}
+
+};
+
+
+class LongSword : public Arme
+{
+	virtual int damage() override
+	{
+		return 10;
+	}
+
+};
+
+
+class LongSword : public Arme
+{
+	virtual int damage() override
+	{
+		return 10;
+	}
+
+};
